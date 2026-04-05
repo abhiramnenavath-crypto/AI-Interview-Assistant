692389e3455ce0ce490bc6bd92004a2e7c58bc84
ABHIRAM <nenavath.abhiram@s.amity.edu>
2026-04-05 14:18:12 +0530

    After adding Voice Output Code
    Implemented voice output using system speech and enhanced feedback logic for answer evaluation

--- Interview_Bot.py
+++ Interview_Bot.py
@@ -1,28 +1,45 @@
-questions = ["What's your name?", "Tell us about yourself: ", "Any Achievements?"]
+import os
+
+def speak(text):
+    text = text.replace("'", "")
+    
+    command = (f'powershell -c "Add-Type –AssemblyName System.Speech; '
+              f'(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'{text}\');"')
+    os.system(command)
+
+questions = ["What is your name?", "Tell us about yourself: ", "Any Achievements?"]
 answers = []
 score = 0
 
 for q in questions:
+
+    speak(q)
+
     ans  = input(q)
     answers.append(ans)
 
-    if q == "What's your name?":
+    if q == "What is your name?":
         continue
 
     words = len(ans.split())
 
     if words < 10:
-        print(f"Your answer for the question '{q}' is very short, please improve it")
+        feedback = f"Your answer for the question '{q}' is very short, please improve it"
         score += 1
     elif words > 150:
-        print(f"Your answer for the question '{q}' is very long, please improve it")
+        feednack = f"Your answer for the question '{q}' is very long, please improve it"
         score += 1
     else :
-        print(f"Your answer for the question '{q}' is perfect.")
+        feedback = f"Your answer for the question '{q}' is perfect."
         score += 2
-        
+    
+    print(feedback)
 
+    speak(feedback)
+    
 
 print("\nThanks for Answering :)")
+speak("Thanks for answering")
 print(f"\nYou'r Confidence score is : {score}/4")
+speak(f"You'r Confidence score is : {score}/4")
 
